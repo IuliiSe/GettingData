66bd3567b23b4785e4fbeec2c626f2450c4d3a98
IuliiSe <IuliiSe@users.noreply.github.com>
2015-10-25 21:18:27 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -5,3 +5,26 @@ fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%2
 download(fileURL, dest="dataset.zip", mode="wb")
 unzip ("dataset.zip", exdir = "./")
 
+#merging the training and the test sets to create one data set
+
+ActivityTrain <- read.table("./Data/UCI HAR Dataset/train/y_train.txt", header=T, sep="\t")
+ActivityTest <- read.table("./Data/UCI HAR Dataset/test/y_test.txt", header=T, sep="\t")
+
+SubjectTrain <- read.table("./Data/UCI HAR Dataset/train/subject_train.txt", header=T, sep="\t")
+SubjectTest <- read.table("./Data/UCI HAR Dataset/test/subject_test.txt", header=T, sep="\t")
+
+DataTrain <- read.table("./Data/UCI HAR Dataset/train/X_train.txt", header=T, sep="\t")
+DataTest <- read.table("./Data/UCI HAR Dataset/test/X_test.txt", header=T, sep="\t")
+
+
+mergedActivity <- merge(ActivityTrain, ActivityTest)
+mergedSubject <- merge(SubjectTrain, SubjectTest)
+mergedData <- merge(DataTrain, DataTest)
+
+dtSubject <- cbind(mergedActivity, mergedSubject)
+MergedTotal <- cbind(dtSubject, mergedData)
+
+# naming the activities in the data set with descriptive activity names  
+
+setnames(mergedDataSubject, "V1", "subject")
+setnames(mergedDataActivity, "V1", "activityNum")
