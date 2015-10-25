410c0ba1e6450133aaf3a520224d6aba3de99205
IuliiSe <IuliiSe@users.noreply.github.com>
2015-10-25 13:36:43 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,6 +2,6 @@
 install.packages("downloader")
 library(downloader)
 fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download(url, dest="dataset.zip", mode="wb") 
+download(fileURL, dest="dataset.zip", mode="wb")
 unzip ("dataset.zip", exdir = "./")
 
